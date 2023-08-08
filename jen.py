de383b48ad391f606fc4206570c1e98b69ad150c
velicharlagokulkumar <velicharlagokulkumar@gmai.com>
2023-08-08 18:25:25 +0530

    commited

--- jen.py
+++ jen.py
@@ -11,7 +11,7 @@ user = server.get_whoami()
 version = server.get_version()
 print('Hello %s from Jenkins %s' % (user['fullName'], version))
 
-change1 poojithaa
+change1 poojithareddy
 
 # #Create deployment jobs
 # #create a blnk job
