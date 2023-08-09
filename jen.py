b364afa4d4e6cf5c84bcf52db05e3cd38e2a018a
velicharlagokulkumar <velicharlagokulkumar@gmai.com>
2023-08-09 14:12:12 +0530

    chaged

--- jen.py
+++ jen.py
@@ -10,7 +10,7 @@ server = jenkins.Jenkins(host, username=username, password=password) #automation
 user = server.get_whoami()
 version = server.get_version()
 print('Hello %s from Jenkins %s' % (user['fullName'], version))
-hi poojitha reddy
+hi poojitha reddy changed
 
 # #Create deployment jobs
 # #create a blnk job
