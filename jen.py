f13f94ed783635ed99bdd5a4c26abfd5404027dd
velicharlagokulkumar <velicharlagokulkumar@gmai.com>
2023-08-10 09:42:39 +0530

    changed

--- jen.py
+++ jen.py
@@ -10,10 +10,31 @@
 #user = server.get_whoami()
 #version = server.get_version()
 #print('Hello %s from Jenkins %s' % (user['fullName'], version))
-a=50
-b=2
-c=a+b
-print(c)
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    
+    while left <= right:
+        mid = left + (right - left) // 2
+        
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    
+    return -1  # Target not found
+
+# Example usage
+sorted_array = [2, 5, 8, 12, 16, 23, 38, 45, 56, 72]
+target_value = 23
+result = binary_search(sorted_array, target_value)
+
+if result != -1:
+    print(f"Target {target_value} found at index {result}")
+else:
+    print("Target not found in the array")
+
 # #Create deployment jobs
 # #create a blnk job
 #cdserver.create_job("job1", jenkins.EMPTY_CONFIG_XML)
